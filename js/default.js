714e2a4d66e12055b9fa5bb5a62727449fd9a0c5
shunsuke-moai <g1527035@st.t-kougei.ac.jp>
2016-06-27 17:23:25 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,1 +1,22 @@
+var number=Math.floor(Math.random()*3);
+var answer=parseInt(window.prompt('数あてゲーム　0~2の数字を入れてね'));
 
+var message;
+if(answer==number)
+{
+  message='あたり';
+}
+else if(answer<number)
+{
+  message='もっと大きいよ';
+}
+else if(answer>number)
+{
+  message='もっと小さいよ';
+}
+else
+{
+  message='0～3の数字で頼むよ';
+}
+
+docment.getElementById('choice').tectContent=message;
